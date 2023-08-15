2d0673188250b611fcef1c56504b906d0c97c383
Denis <denis.argatski@gmail.com>
2023-08-15 23:18:41 +0300

    main.c

---
+++ Lesson13-W3D4/02.Ex_CompilationStep2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define  N 999
+
+int main(int argc, char **argv)
+{
+    //Example comment
+    int d =100;
+    d = d+1;
+printf("HELLO WORLD %d, %d\n",d,N);
+
+return 0;
+
+}
